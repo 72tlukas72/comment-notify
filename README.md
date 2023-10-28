a2c0b72b091ec0ea7dc634675054e43e0729e745
72tlukas72 <149176237+72tlukas72@users.noreply.github.com>
2023-10-28 18:40:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# comments notify
+Plugin přidává možnost upozornění administrátora na nový komentář.
+ 
+## Requirements
+- SunLight CMS 8
+
+## Installation
+
+- Copy the folder plugins and its contents to the root directory
+
+or
+ 
+- Installation via administration: Administration > Plugins > Upload new plugins
+
+## Configuration
+
+- Configure e-mail address in: Administration > Plugins > Upozornění na nový komentář
---
+++ plugins/extend/comment-notify/event/comment_notify.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Sunlight;
+
+use Sunlight\Database\Database as DB;
+use Sunlight\Email;
+
+return function(array $args) {
+    $mailText = "Dobrý den,\nna webové stránky ".Template::siteTitle()." (".Template::siteUrl().")";
+    $send = false;
+    $user = "";
+    
+    if($args['posttype'] == 1 || $args['posttype'] == 3 || $args['posttype'] == 5)
+    {
+        $title = DB::result(DB::query("SELECT title FROM " . DB::table('page') . "  WHERE id=".$args['posttarget']));
+        if($args['posttype'] == 1) $mailText .= " byl k sekci ".$title." přidán nový příspěvek ";
+        if($args['posttype'] == 3) $mailText .= " byl ke knize ".$title." přidán nový příspěvek ";
+        if($args['posttype'] == 5) $mailText .= " bylo k fóru ".$title." přidáno nové téma s předmětem ".$args['subject'];
+        $send = true;
+    }
+    if($args['posttype'] == 2)
+    {
+        $title = DB::result(DB::query("SELECT title FROM " . DB::table('article') . " WHERE id=".$args['posttarget']), 0);
+        $mailText .= " byl k článku ".$title." přidán nový komentář";
+        $send = true;
+    }
+    if($send)
+    {
+        if($args['author'] == -1) $mailText .= " od hosta ".$args['guest'].".\n";
+        else
+        {
+            $user = DB::result(DB::query("SELECT username FROM " . DB::table('user') . " WHERE id=".$args['author']), 0);
+            $mailText .= " od uživatele ".$user.".\n";
+        }
+
+        for($i = 0; $i < 30; $i++)
+            $mailText .= "-";
+ 
+        $mailText .= "\n".$args['text'];   
+    
+        if (!$this->getConfig()['adresát_upozornění'] || $this->getConfig()['adresát_upozornění'] == "defaul@mail.com") 
+            return;
+        else
+            Email::send($this->getConfig()['adresát_upozornění'], "Nový příspěvek na webu", $mailText);
+    }
+};
