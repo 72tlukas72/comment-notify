c62368d239960c2979a247594d8e590d82a56c49
Jirka Daněk <jirka.danek@jdanek.eu>
2023-11-01 23:59:17 +0100

    CS fix

--- plugins/extend/comment-notify/event/comment_notify.php
+++ plugins/extend/comment-notify/event/comment_notify.php
@@ -12,38 +12,38 @@ return function(array $args) {
         return;
     }
     
-    $mailText = "Dobrý den,\nna webové stránky ".Template::siteTitle()." (".Template::siteUrl().")";
+    $mailText = "Dobrý den,\nna webové stránky " . Template::siteTitle() . " (" . Template::siteUrl() . ")";
     $send = false;
     $user = "";
     
     if($args['posttype'] == Post::SECTION_COMMENT || $args['posttype'] == Post::BOOK_ENTRY || $args['posttype'] == Post::FORUM_TOPIC) {
-        $title = DB::result(DB::query("SELECT title FROM " . DB::table('page') . "  WHERE id=".$args['posttarget']));
-        if($args['posttype'] == Post::SECTION_COMMENT) $mailText .= " byl k sekci ".$title." přidán nový příspěvek ";
-        if($args['posttype'] == Post::BOOK_ENTRY) $mailText .= " byl ke knize ".$title." přidán nový příspěvek ";
-        if($args['posttype'] == Post::FORUM_TOPIC) $mailText .= " bylo k fóru ".$title." přidáno nové téma s předmětem ".$args['subject'];
+        $title = DB::result(DB::query("SELECT title FROM " . DB::table('page') . "  WHERE id=" . $args['posttarget']));
+        if($args['posttype'] == Post::SECTION_COMMENT) $mailText .= " byl k sekci " . $title . " přidán nový příspěvek ";
+        if($args['posttype'] == Post::BOOK_ENTRY) $mailText .= " byl ke knize " . $title . " přidán nový příspěvek ";
+        if($args['posttype'] == Post::FORUM_TOPIC) $mailText .= " bylo k fóru " . $title . " přidáno nové téma s předmětem " . $args['subject'];
         $send = true;
     }
 
     if($args['posttype'] == Post::ARTICLE_COMMENT) {
-        $title = DB::result(DB::query("SELECT title FROM " . DB::table('article') . " WHERE id=".$args['posttarget']), 0);
-        $mailText .= " byl k článku ".$title." přidán nový komentář";
+        $title = DB::result(DB::query("SELECT title FROM " . DB::table('article') . " WHERE id=" . $args['posttarget']), 0);
+        $mailText .= " byl k článku " . $title . " přidán nový komentář";
         $send = true;
     }
 
     if ($send) {
         if ($args['author'] == -1) {
-            $mailText .= " od hosta ".$args['guest'].".\n";
+            $mailText .= " od hosta " . $args['guest'] . ".\n";
         } else {
-            $user = DB::result(DB::query("SELECT username FROM " . DB::table('user') . " WHERE id=".$args['author']), 0);
-            $mailText .= " od uživatele ".$user.".\n";
+            $user = DB::result(DB::query("SELECT username FROM " . DB::table('user') . " WHERE id=" . $args['author']), 0);
+            $mailText .= " od uživatele " . $user . ".\n";
         }
 
         for($i = 0; $i < 30; $i++) {
             $mailText .= "-";
         }
  
-        $mailText .= "\n".$args['text'];   
+        $mailText .= "\n" . $args['text'];   
     
-        Email::send($this->getConfig()['adresát_upozornění'], "Nový příspěvek na webu", $mailText);
+        Email::send($this->getConfig()['recipient'], "Nový příspěvek na webu", $mailText);
     }
 };
