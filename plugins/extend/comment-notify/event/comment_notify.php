9ff8012c87ac1a169bdb2c8a58c65c6f72034057
Jirka Daněk <jirka.danek@jdanek.eu>
2023-11-01 23:53:58 +0100

    Replace numeric types with constants

--- plugins/extend/comment-notify/event/comment_notify.php
+++ plugins/extend/comment-notify/event/comment_notify.php
@@ -4,6 +4,7 @@ namespace Sunlight;
 
 use Sunlight\Database\Database as DB;
 use Sunlight\Email;
+use Sunlight\Post\Post;
 
 return function(array $args) {
     
@@ -15,31 +16,31 @@ return function(array $args) {
     $send = false;
     $user = "";
     
-    if($args['posttype'] == 1 || $args['posttype'] == 3 || $args['posttype'] == 5)
-    {
+    if($args['posttype'] == Post::SECTION_COMMENT || $args['posttype'] == Post::BOOK_ENTRY || $args['posttype'] == Post::FORUM_TOPIC) {
         $title = DB::result(DB::query("SELECT title FROM " . DB::table('page') . "  WHERE id=".$args['posttarget']));
-        if($args['posttype'] == 1) $mailText .= " byl k sekci ".$title." přidán nový příspěvek ";
-        if($args['posttype'] == 3) $mailText .= " byl ke knize ".$title." přidán nový příspěvek ";
-        if($args['posttype'] == 5) $mailText .= " bylo k fóru ".$title." přidáno nové téma s předmětem ".$args['subject'];
+        if($args['posttype'] == Post::SECTION_COMMENT) $mailText .= " byl k sekci ".$title." přidán nový příspěvek ";
+        if($args['posttype'] == Post::BOOK_ENTRY) $mailText .= " byl ke knize ".$title." přidán nový příspěvek ";
+        if($args['posttype'] == Post::FORUM_TOPIC) $mailText .= " bylo k fóru ".$title." přidáno nové téma s předmětem ".$args['subject'];
         $send = true;
     }
-    if($args['posttype'] == 2)
-    {
+
+    if($args['posttype'] == Post::ARTICLE_COMMENT) {
         $title = DB::result(DB::query("SELECT title FROM " . DB::table('article') . " WHERE id=".$args['posttarget']), 0);
         $mailText .= " byl k článku ".$title." přidán nový komentář";
         $send = true;
     }
-    if($send)
-    {
-        if($args['author'] == -1) $mailText .= " od hosta ".$args['guest'].".\n";
-        else
-        {
+
+    if ($send) {
+        if ($args['author'] == -1) {
+            $mailText .= " od hosta ".$args['guest'].".\n";
+        } else {
             $user = DB::result(DB::query("SELECT username FROM " . DB::table('user') . " WHERE id=".$args['author']), 0);
             $mailText .= " od uživatele ".$user.".\n";
         }
 
-        for($i = 0; $i < 30; $i++)
+        for($i = 0; $i < 30; $i++) {
             $mailText .= "-";
+        }
  
         $mailText .= "\n".$args['text'];   
     
