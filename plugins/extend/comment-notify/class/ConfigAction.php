11c530cc4bfa716e5843b35bdcc10b9bb1f0e5ea
Jirka Daněk <jirka.danek@jdanek.eu>
2023-11-02 18:40:49 +0100

    Improve configuration form

---
+++ plugins/extend/comment-notify/class/ConfigAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SunlightExtend\CommentNotify;
+
+use Sunlight\Plugin\Action\ConfigAction as BaseConfigAction;
+
+class ConfigAction extends BaseConfigAction
+{
+    
+    public function getConfigLabel(string $key): string
+    {
+        return _lang('comment-notify.config.' . $key);
+    }
+}
---
+++ plugins/extend/comment-notify/lang/cs.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'config.recipient' => 'Adresát',
+];
