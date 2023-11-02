eeacae223651d3e3d29c067148fd45155b7320b6
72tlukas72 <149176237+72tlukas72@users.noreply.github.com>
2023-11-02 20:17:26 +0100

    Fixing language translations

--- plugins/extend/comment-notify/class/ConfigAction.php
+++ plugins/extend/comment-notify/class/ConfigAction.php
@@ -6,7 +6,6 @@ use Sunlight\Plugin\Action\ConfigAction as BaseConfigAction;
 
 class ConfigAction extends BaseConfigAction
 {
-    
     public function getConfigLabel(string $key): string
     {
         return _lang('comment-notify.config.' . $key);
